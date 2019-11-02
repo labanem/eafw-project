2506ab3278bd6304aaca576e9ebdc861dd53c40e
labanem <labanem@yahoo.com>
2019-11-02 10:03:06 +0300

    company model edit

--- app/compTb.php
+++ app/compTb.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class compTb extends Model
 {
-    //
+    public function worker()
+    {
+    	return $this->hasMany('App\empDetailsTb', 'compid');
+    }
 }
